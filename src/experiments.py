dab4efed70235fad1730b943494d258df862746a
jlapeyra <jlapeyraa@gmail.com>
2025-11-30 18:53:02 +0100

    python implementat / experiments fets

---
+++ src/experiments.py
@@ -0,0 +1,54 @@
+from io import TextIOWrapper
+from graph_generator import *
+from typing import Type
+import os
+from datetime import datetime
+import shutil
+import tqdm
+
+def run(generator: Type[GraphGenerator]):
+    print(f"Running experiment for generator: {generator.name}")
+    t_saved = [1, 10, 100, 1000]
+    dir_path = f"results/{generator.dir_name}"
+    file_template = dir_path + "/" + "timeseries_t{}.txt"
+    os.makedirs(dir_path, exist_ok=True)
+
+    for t in t_saved:
+        file_path = file_template.format(t)
+        if os.path.exists(file_path):
+            print(f"WARNING: Ja existeix el fitxer {file_path}.")
+            old_dir = os.path.join(dir_path, "old")
+            os.makedirs(old_dir, exist_ok=True)
+            mtime = os.path.getmtime(file_path)
+            timestamp = datetime.fromtimestamp(mtime).strftime("%Y%m%d-%H%M%S")
+            base = os.path.splitext(os.path.basename(file_path))[0]
+            new_path = os.path.join(old_dir, f"{base}-{timestamp}.txt")
+            shutil.move(file_path, new_path)
+            print(f"Moved existing file to {new_path}.")
+    files: dict[int, TextIOWrapper] = {}
+    try:
+        for t in t_saved:
+            file_path = file_template.format(t)
+            files[t] = open(file_path, "w")
+
+        for T, G in tqdm.tqdm(generator.generate(), total=Constants.T_MAX):
+            for t, f in files.items():
+                if T >= t:
+                    f.write(f"{T} {G.degree[t]}\n")
+        last_G = G
+    finally:
+        for f in files.values():
+            f.close()
+            print(f"Closed file {f.name}.")
+
+    G = last_G
+    degrees = sorted([d for n, d in G.degree()], reverse=True)
+    with open(os.path.join(dir_path, "degrees_tmax.txt"), "w") as f:
+        for d in degrees:
+            f.write(f"{d}\n")
+        print(f"Saved degree distribution to {f.name}.")
+
+if __name__ == "__main__":
+    classes = [BA_StaticNodes]
+    for cls in classes:
+        run(cls)
---
+++ src/graph_generator.py
@@ -0,0 +1,138 @@
+import random
+import networkx as nx
+from typing import Iterable
+
+class Constants:
+    N0 = 10
+    M0 = 10
+    T_MAX = 10 ** 5
+
+class GraphGenerator():
+
+    name: str
+    dir_name: str
+
+    @staticmethod
+    def generate(        
+        n0: int = Constants.N0,
+        m0: int = Constants.M0,
+        t_max: int = Constants.T_MAX,
+        *,
+        seed: int | None = None
+        ) -> Iterable[tuple[int, nx.Graph]]:
+        """
+        Paràmetres
+        ----------
+        n0 : int
+            Nombre inicial de vèrtexs (al temps t = 0).
+        m0 : int
+            Nombre d'arestes que afegeix cada nou vèrtex (m0 >= 1).
+        t_max : int
+            Nombre de passos de temps (nombre de vèrtexs nous que afegirem).
+            El nombre final de vèrtexs serà n0 + t_max.
+        seed : int | None
+            Llavor per al generador aleatori (opcional, per reproductibilitat).
+
+        Retorna (t, G) a cada iteració, on t és el temps actual i G és el graf generat fins al moment.
+        """
+        raise NotImplementedError()
+    
+    def init(n0: int, m0: int, t_max:int, seed: int | None = None) -> None:
+        random.seed(seed)
+        if m0 < 1:    raise ValueError("m0 ha de ser >= 1")
+        if n0 < m0:   raise ValueError("Cal que n0 >= m0 per poder connectar el nou vèrtex a m0 vèrtexs diferents.")
+        if t_max < 0: raise ValueError("t_max ha de ser >= 0")
+
+class BarabasiAlbert(GraphGenerator):
+    name = "Barabási-Albert Preferential Attachment"
+    dir_name = "barabasi_albert"
+    @staticmethod
+    def generate(
+        n0: int = Constants.N0,
+        m0: int = Constants.M0,
+        t_max: int = Constants.T_MAX,
+        *,
+        seed: int | None = None
+    ) -> Iterable[tuple[int, nx.Graph]]:
+
+        GraphGenerator.init(n0, m0, t_max, seed)
+
+        G = nx.Graph()
+        G.add_nodes_from(range(-n0, 0)) # Vèrtexs inicials: -n0, ..., -1
+
+        stubs = []
+        for t in range(t_max):
+            new_v = t
+
+            if len(stubs) >= m0:
+                targets = random.sample(stubs, k=m0)
+            else:
+                targets = random.sample(stubs + list(G.nodes), k=m0)
+
+            G.add_node(new_v)
+
+            for u in targets:
+                G.add_edge(new_v, u)
+                stubs.append(new_v)
+                stubs.append(u)
+
+            yield (t, G)
+
+class BA_RandomAttachment(GraphGenerator):
+    name = "Barabási-Albert Random Attachment"
+    dir_name = "ba_random_attachment"
+    @staticmethod
+    def generate(
+        n0: int = Constants.N0,
+        m0: int = Constants.M0,
+        t_max: int = Constants.T_MAX,
+        *,
+        seed: int | None = None
+    ) -> Iterable[tuple[int, nx.Graph]]:
+
+        GraphGenerator.init(n0, m0, t_max, seed)
+
+        G = nx.Graph()
+        G.add_nodes_from(range(-n0, 0)) # Vèrtexs inicials: -n0, ..., -1
+
+        for t in range(t_max):
+            new_v = t
+            targets = random.sample(list(G.nodes), k=m0)
+            G.add_node(new_v)
+            for u in targets:
+                G.add_edge(new_v, u)
+            yield (t, G)
+
+class BA_StaticNodes(GraphGenerator):
+    name = "Barabási-Albert Static Nodes"
+    dir_name = "ba_static_nodes"
+
+    @staticmethod
+    def generate(
+        n0: int = None,
+        m0: int = Constants.M0,
+        t_max: int = Constants.T_MAX,
+        *,
+        seed: int | None = None
+    ) -> Iterable[tuple[int, nx.Graph]]:
+
+        if n0 is None:
+            n0 = m0*t_max
+
+        GraphGenerator.init(n0, m0, t_max, seed)
+
+        G = nx.Graph()
+        G.add_nodes_from(range(0, n0)) # Vèrtexs: 0, ..., n0-1
+
+        stubs = []
+        for t in range(t_max):
+            v = t
+            if len(stubs) >= m0:
+                targets = random.sample(stubs, k=m0)
+            else:
+                targets = random.sample(stubs + list(G.nodes), k=m0)
+            for u in targets:
+                G.add_edge(v, u)
+                stubs.append(v)
+                stubs.append(u)
+            yield (t, G)
